e01c0b14969f4b62c0535298c7cfd4b8f34990a8
unknown <rudenko.sergey92@gmail.com>
2013-07-21 00:32:34 +0300

    changes to web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
-var express = require('express');
+var express = require('express')
+    ,fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buffer = new Buffer(fs.readFileSync('index.html'));
+    response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
