8b2f0d20875e188caaf7709cb0a881db93e25181
unknown <rudenko.sergey92@gmail.com>
2013-07-21 15:11:38 +0300

    change index for hw4

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    var buffer = new Buffer(fs.readFileSync('index.html'));
-    response.send(buffer.toString('utf-8'));
+    var indexhtml  = fs.readFileSync('index.html', 'utf-8');
+    response.send(indexhtml);
 });
 
 var port = process.env.PORT || 3000;
